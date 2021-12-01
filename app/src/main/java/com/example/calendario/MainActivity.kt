fd56ae1d97315dae057c1d70f32120fd1428e17b
luissancar <luissancar@yahoo.es>
2021-12-01 11:00:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/calendario/MainActivity.kt
@@ -0,0 +1,141 @@
+package com.example.calendario
+
+import com.example.calendario.ui.theme.CalendarioTheme
+
+
+
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalendarioTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    DateView()
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("UnusedTransitionTargetStateParameter")
+@Composable
+fun DateView() {
+
+
+
+
+    var expanded by remember { mutableStateOf(false) }
+    val transitionState = remember { MutableTransitionState(expanded).apply {
+        targetState = !expanded
+    }}
+    val transition = updateTransition(targetState = transitionState, label = "transition")
+    val arrowRotationDegree by transition.animateFloat({
+        tween(durationMillis = 300)
+    }, label = "rotationDegree") {
+        if (expanded) 180f else 0f
+    }
+    val context = LocalContext.current
+    val viewModel: SpinnerViewModel = viewModel()
+    val dateTime = viewModel.time.observeAsState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Blue)
+                .padding(15.dp)
+        ) {
+            Text(
+                text = "DateTime",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                color = Color.White
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Column {
+
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.5f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Calender Date ",
+                    fontSize = 20.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                TextButton(
+                    onClick = {
+                        viewModel.selectDateTime(context)
+                    },
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(Color.Blue)
+                        .padding(5.dp)
+                ) {
+                    Text(text = "Select Date", color = Color.White)
+                }
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Text(text = dateTime.value ?: "No Time Set")
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
